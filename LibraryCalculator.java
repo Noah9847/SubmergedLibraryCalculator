78a840a09b34371ec22a39d293825038a523d347
Noah9847 <70857168+Noah9847@users.noreply.github.com>
2022-04-21 21:05:17 -0500

    Update LibraryCalculator.java

--- LibraryCalculator.java
+++ LibraryCalculator.java
@@ -9,6 +9,7 @@ public class LibraryCalculator {
     String middleDoor;
     String rightDoor;
     boolean floorThere;
+    int[] answer;
 
     String[][] library = {{"n", "r", "b", "y", "y", "y", "b", "n"}, 
                           {"n", "b", "b", "y", "g", "b", "b", "n"}, 
@@ -23,6 +24,29 @@ public class LibraryCalculator {
         this.middleDoor = middleDoor;
         this.rightDoor = rightDoor;
         this.floorThere = floorThere;
+        // this.answer = answer;
+    }
+
+    public void instructionOutput(){
+        // System.out.println(answer[0] + " " + answer[1]);
+        if (answer[1] == 1){
+            System.out.println("first door down (from left)");
+        }
+        else if (answer[1] == 2){
+            System.out.println("second door down (from left)");
+        }
+        else if (answer[1] == 3){
+            System.out.println("third door down (from left)");
+        }
+        else if (answer[1] == 4){
+            System.out.println("fourth door down (from left)");
+        }
+        else if (answer [1] == 5){
+            System.out.println("fifth door down (from left)");
+        }
+        else if (answer[1] == 6){
+            System.out.println("sixth door down (from left)");
+        }
     }
     
     public int[] doorFinder(){
@@ -30,10 +54,10 @@ public class LibraryCalculator {
         int topRow = 0;
         boolean foundTheDoor = false;
         int[] answer = new int[2];
-        System.out.println(answer[0] + " " + answer[1]);
+        // System.out.println(answer[0] + " " + answer[1]);
 
         for (int i = 0; i < library.length - 1; i++){
-            System.out.println("1st for loop started");
+            // System.out.println("1st for loop started");
             for (int j = 0; j < 6; j++){
                // System.out.println("2nd for loop started");
                // System.out.println(library[i][j] + " " + leftDoorTop);
@@ -41,8 +65,8 @@ public class LibraryCalculator {
                // System.out.println(library[i][j+2] + " " + rightDoorTop);
                // System.out.println(library[i][j].equals(leftDoorTop));
               if (library[i][j].equals(leftDoorTop) && library[i][j+1].equals(middleDoorTop) && library[i][j+2].equals(rightDoorTop)) {
-                System.out.println(library[i][j+1]);
-                System.out.println(i);
+                // System.out.println(library[i][j+1]);
+                // System.out.println(i);
                 i = topRow;
                 if (floorThere == false){
                     answer[0] = i;
@@ -54,9 +78,9 @@ public class LibraryCalculator {
                 for (int a = i + 1; a < library.length - i + 1; a++){
                     for (int b = 0; b < 6; b++){
                         if (library[a][b].equals(leftDoor) && library[a][b+1].equals(middleDoor) && library[a][b+2].equals(rightDoor)){
-                            System.out.println(library[a][b+1]);
-                            System.out.println(a);
-                            System.out.println("Found both doors");
+                            // System.out.println(library[a][b+1]);
+                            // System.out.println(a);
+                            // System.out.println("Found both doors");
                             answer[0] = a;
                             answer[1] = b + 1;
                             foundTheDoor = true;
@@ -83,7 +107,7 @@ public class LibraryCalculator {
 
         LibraryCalculator calculator = new LibraryCalculator("", "", "", "", "", "", true);
         Scanner scanner = new Scanner(System.in);
-        System.out.println("test");
+        // System.out.println("test");
         System.out.println("Initialized. The following print statements will tell you what to enter. When it says to enter the door, enter the color of the door as r, b, y, or g depending on the color of the door. If there is no door to the left or right of you, press n");
         System.out.println();
         System.out.println("Is there a floor above you? If yes, enter true. If no, enter false");
@@ -118,7 +142,28 @@ public class LibraryCalculator {
         }
         
         int[] answer = calculator.doorFinder();
-        System.out.println(answer[0] + " " + answer[1]);
+        calculator.answer = answer;
+
+        System.out.println();
+
+        if (answer[0] == 0){
+            System.out.print("Top level, ");
+            calculator.instructionOutput();
+        }
+        else if (answer[0] == 1){
+            System.out.print("second level down from top, ");
+            calculator.instructionOutput();
+        }
+        else if (answer[0] == 2){
+            System.out.print("third level down from the top, ");
+            calculator.instructionOutput();
+        }
+        else if (answer[0] == 3){
+            System.out.print("bottom level, ");
+            calculator.instructionOutput();
+        }
+
+        System.out.println("The door color is " + calculator.library[answer[0]][answer[1]]);
 
         scanner.close();
 
